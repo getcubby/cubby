b588e9dcfdb8e9b689b11441f2efbd9374a41b37
Johannes Zellner <johannes@nebulon.de>
2024-09-30 18:43:53 +0200

    Ensure we kick the indexer if groupfolders change

--- backend/groupfolders.js
+++ backend/groupfolders.js
@@ -15,7 +15,8 @@ const assert = require('assert'),
     database = require('./database.js'),
     fs = require('fs-extra'),
     path = require('path'),
-    MainError = require('./mainerror.js');
+    MainError = require('./mainerror.js'),
+    recoll = require('./recoll.js');
 
 function postProcess(data) {
     data.folderPath = data.folder_path;
@@ -61,6 +62,11 @@ async function add(idOrSlug, name, folderPath = '', members = []) {
     // ensure folder
     if (!folderPath) folderPath = path.join(constants.GROUPS_DATA_ROOT, idOrSlug);
     fs.mkdirSync(folderPath, { recursive: true });
+
+    // kick off indexer in background
+    for (const username of members) {
+        recoll.indexByUsername(username);
+    }
 }
 
 async function get(id) {
@@ -135,6 +141,9 @@ async function update(id, name, members) {
         if (error.nestedError && error.nestedError.constraint === 'groupfolders_pkey') throw new MainError(MainError.ALREADY_EXISTS, 'groupFolder already exists');
         throw error;
     }
+
+    // FIXME reindex for all for the moment until we know who got removed!
+    recoll.index();
 }
 
 async function remove(id) {
@@ -159,6 +168,9 @@ async function remove(id) {
     }];
 
     await database.transaction(queries);
+
+    // FIXME reindex for all for the moment until we know who got removed!
+    recoll.index();
 }
 
 function isPartOf(groupFolder, username) {

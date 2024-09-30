b0692b07fa780ba8e2d5a250f60b9f4813eaa11b
Johannes Zellner <johannes@nebulon.de>
2024-09-30 22:48:55 +0200

    path.resolve() can't take undefined

--- backend/constants.js
+++ backend/constants.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 exports = module.exports = {
-    SESSION_SECRET_FILE_PATH: path.resolve(process.env.SESSION_SECRET_FILE_PATH) || path.resolve(__dirname, '../.secret'),
-    USER_DATA_ROOT: path.resolve(process.env.USER_DATA_PATH) || path.resolve(__dirname, '../.data'),
-    GROUPS_DATA_ROOT: path.resolve(process.env.GROUPS_DATA_PATH) || path.resolve(__dirname, '../.groups'),
-    THUMBNAIL_ROOT: path.resolve(process.env.THUMBNAIL_PATH) || path.resolve(__dirname, '../.thumbnails'),
-    SESSION_PATH: path.resolve(process.env.SESSION_PATH) || path.resolve(__dirname, '../.sessions'),
-    SEARCH_INDEX_PATH: path.resolve(process.env.SEARCH_INDEX_PATH) || path.resolve(__dirname, '../.recoll'),
+    SESSION_SECRET_FILE_PATH: process.env.SESSION_SECRET_FILE_PATH ? path.resolve(process.env.SESSION_SECRET_FILE_PATH) : path.resolve(__dirname, '../.secret'),
+    USER_DATA_ROOT: process.env.USER_DATA_PATH ? path.resolve(process.env.USER_DATA_PATH) : path.resolve(__dirname, '../.data'),
+    GROUPS_DATA_ROOT: process.env.GROUPS_DATA_PATH ? path.resolve(process.env.GROUPS_DATA_PATH) : path.resolve(__dirname, '../.groups'),
+    THUMBNAIL_ROOT: process.env.THUMBNAIL_PATH ? path.resolve(process.env.THUMBNAIL_PATH) : path.resolve(__dirname, '../.thumbnails'),
+    SESSION_PATH: process.env.SESSION_PATH ? path.resolve(process.env.SESSION_PATH) : path.resolve(__dirname, '../.sessions'),
+    SEARCH_INDEX_PATH: process.env.SEARCH_INDEX_PATH ? path.resolve(process.env.SEARCH_INDEX_PATH) : path.resolve(__dirname, '../.recoll'),
 };

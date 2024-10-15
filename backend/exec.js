4634a11c55e15fb75b66b4633d3ca2ef3bfef01e
Johannes Zellner <johannes@nebulon.de>
2024-10-15 12:31:38 +0200

    Fixes for child_process.spawn()

--- backend/exec.js
+++ backend/exec.js
@@ -21,13 +21,13 @@ exports = module.exports = function exec(command, args, options) {
         // p.stdout and p.stderr may be null if stdio option is non-default
         if (p.stdout) {
             p.stdout.on('data', (d) => {
-                stdout += d;
+                stdout += d.toString('utf8');
             });
         }
 
         if (p.stderr) {
             p.stderr.on('data', (d) => {
-                stderr += d;
+                stderr += d.toString('utf8');
             });
         }
 
@@ -39,9 +39,13 @@ exports = module.exports = function exec(command, args, options) {
             e.stderr = stderr; // when promisified, this is the way to get stderr
             e.code = code;
 
-            debug(`${command} with args ${args.join(' ')} errored`, e);
+            debug(`${command} with args ${args.join(' ')} exited with error`, e);
 
             reject(e);
+        });
+
+        p.on('error', function (error) { // when the command itself could not be started
+            debug(`${command} with args ${args.join(' ')} errored`, error);
         });
     });
 };

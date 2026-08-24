7bc902435b8a599c9cf8463ec061065c26b0f9f4
Johannes Zellner <johannes@cloudron.io>
2026-08-24 22:18:24 +0000

    temporarily disable update test

--- test/test.js
+++ test/test.js
@@ -67,19 +67,19 @@ describe('Application life cycle test', function () {
 
   it('uninstall app', cloudronCli.uninstall);
 
-  it('install app for update', cloudronCli.appstoreInstall);
+  // it('install app for update', cloudronCli.appstoreInstall);
 
-  it('can login', login);
-  it('can see Welcome.md', checkWelcomeMd);
-  it('can create a new file', createTestFile);
-  it('can logout', logout);
+  // it('can login', login);
+  // it('can see Welcome.md', checkWelcomeMd);
+  // it('can create a new file', createTestFile);
+  // it('can logout', logout);
 
-  it('can update', cloudronCli.update);
+  // it('can update', cloudronCli.update);
 
-  it('can login', login);
-  it('can see Welcome.md', checkWelcomeMd);
-  it('can see home files', checkHomeFiles);
-  it('can logout', logout);
+  // it('can login', login);
+  // it('can see Welcome.md', checkWelcomeMd);
+  // it('can see home files', checkHomeFiles);
+  // it('can logout', logout);
 
-  it('uninstall app', cloudronCli.uninstall);
+  // it('uninstall app', cloudronCli.uninstall);
 });
